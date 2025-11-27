f5a3698a1790effdabeb61036142f7eb6941d3e7
umarm105 <paalandtapin@gmail.com>
2025-11-26 23:56:27 -0500

    Insertion of elements (players) data

--- db/insert_data.py
+++ db/insert_data.py
@@ -16,9 +16,7 @@ DB_PARAMS = {
     "port": int(os.getenv("DB_PORT", 5432))
 }
 
-def insert_teams_to_postgres(teams):
-    conn = psycopg2.connect(**DB_PARAMS)
-    cur = conn.cursor()
+def insert_teams_to_postgres(teams, cur):
     for team in teams:
         cur.execute("""
             INSERT INTO teams (
@@ -34,16 +32,37 @@ def insert_teams_to_postgres(teams):
             )
             ON CONFLICT (code) DO NOTHING;
         """, team)
-    conn.commit()
-    cur.close()
-    conn.close()
 
-def fetch_and_insert_teams():
+def insert_elements_to_postgres(elements, cur):
+    for el in elements:
+        cur.execute("""
+            INSERT INTO elements (
+                id, code, first_name, second_name, web_name, team, team_code, element_type, now_cost,
+                points_per_game, total_points, minutes, goals_scored, assists, clean_sheets, goals_conceded,
+                own_goals, penalties_saved, penalties_missed, yellow_cards, red_cards, saves, bonus, bps,
+                influence, creativity, threat, ict_index, form, status, birth_date, region, team_join_date
+            ) VALUES (
+                %(id)s, %(code)s, %(first_name)s, %(second_name)s, %(web_name)s, %(team)s, %(team_code)s, %(element_type)s, %(now_cost)s,
+                %(points_per_game)s, %(total_points)s, %(minutes)s, %(goals_scored)s, %(assists)s, %(clean_sheets)s, %(goals_conceded)s,
+                %(own_goals)s, %(penalties_saved)s, %(penalties_missed)s, %(yellow_cards)s, %(red_cards)s, %(saves)s, %(bonus)s, %(bps)s,
+                %(influence)s, %(creativity)s, %(threat)s, %(ict_index)s, %(form)s, %(status)s, %(birth_date)s, %(region)s, %(team_join_date)s
+            )
+            ON CONFLICT (id) DO NOTHING;
+        """, el)
+
+def fetch_and_insert_data():
     response = requests.get(FPL_API_URL)
     response.raise_for_status()
     data = response.json()
     teams = data.get("teams", [])
-    insert_teams_to_postgres(teams)
+    elements = data.get("elements", [])
+    conn = psycopg2.connect(**DB_PARAMS)
+    cur = conn.cursor()
+    insert_teams_to_postgres(teams, cur)
+    insert_elements_to_postgres(elements, cur)
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
-    fetch_and_insert_teams()
+    fetch_and_insert_data()
